f50a328df0f6ed02e826fb689ee4f6ab61a5b443
Aleson12 <felizardoaleson@yahoo.com>
2025-01-21 17:10:33 -0300

    Atualização feita pelo usuário B na Classe Y.

--- src/view/ClasseY.java
+++ src/view/ClasseY.java
@@ -2,6 +2,6 @@ package view;
 
 public class ClasseY {
 	public static void main(String[] args) {
-		System.out.println("Usuário B altera esta classe, Y.");
+		System.out.println("'Usuário B' alterando esta classe agora (apenas um teste).");
 	}
 }
